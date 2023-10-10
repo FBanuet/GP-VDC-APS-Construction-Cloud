4922b95c8557a7475f21ac5c1dc6052a882c9bb0
DATArchitects NET <banarq_data@outlook.com>
2023-10-10 09:56:10 -0600

    pull committed

--- wwwroot/extensions/DataGridPanel.js
+++ wwwroot/extensions/DataGridPanel.js
@@ -1,34 +1,32 @@
 const DATAGRID_CONFIG = {
-    requiredProps: ['name','Category','VDC_EJES','VDC_PRECO','VDC_PRIMARY_UNITS','VDC_TYPE_NAME','VDC_SECUNDARY_UNITS','VDC_SECTION_SHAPE','VDC_LENGTH','VDC_WEIGHT'], // Which properties should be requested for each object
+    requiredProps: ['name','Category','VDC_M/PUL','VDC_PRECO','Comments','VDC_IMCA','VDC_AISC','VDC_LENGTH','VDC_WEIGHT'], // Which properties should be requested for each object
     responsiveLayout:true,
     
     columns: [ // Definition of individual grid columns (see http://tabulator.info for more details)
         { title: 'ID', field: 'dbid',responsive:3 },
-        { title: 'VDC_TYPE_NAME', field: 'type_name', width: 180,responsive:0 },
+        { title: 'Comments', field: 'partida', width: 180,responsive:0 },
         { title: 'Name', field: 'name', width: 180,responsive:0 },
         { title: 'Category', field: 'category',responsive:0  },
-        {title: 'VDC_EJES',field: 'EJES',responsive:2},
+        {title: 'VDC_M/PUL',field: 'MassDensity',responsive:2},
         {title: 'VDC_PRECO',field: 'preco',responsive:1},
-        {title: 'VDC_PRIMARY_UNITS',field:'units',responsive:0},
-        {title: 'VDC_SECUNDARY_UNITS',field:'s_units',responsive:0},
-        {title: 'VDC_SECTION_SHAPE',field:'section',responsive:2},
+        {title: 'VDC_IMCA',field:'imca',responsive:0},
+        {title: 'VDC_AISC',field:'section',responsive:2},
         {title: 'VDC_LENGTH',field:'Length',responsive:0,topCalc:"sum", topCalcParams:{precision:1,}},
         {title: 'VDC_WEIGHT',field: 'weight',responsive:0 ,topCalc:"sum", topCalcParams:{precision:1,}},
         
     ],
-    groupBy: 'EJES', // Optional column to group by
+    groupBy: 'partida', // Optional column to group by
     createRow: (dbid, name, props) => { // Function generating grid rows based on recieved object properties
         const category = props.find(p => p.displayName === 'Category')?.displayValue;
-        const EJES = props.find(p => p.displayName === 'VDC_EJES')?.displayValue;
+        const mass = props.find(p => p.displayName === 'VDC_M/PUL')?.displayValue;
         const preco = props.find(z => z.displayName === 'VDC_PRECO')?.displayValue.toString();
-        const units = props.find(z => z.displayName === 'VDC_PRIMARY_UNITS')?.displayValue;
-        const s_units = props.find(z => z.displayName === 'VDC_SECUNDARY_UNITS')?.displayValue;
-        const section = props.find(c => c.displayName === 'VDC_SECTION_SHAPE')?.displayValue;
+        const Imca = props.find(z => z.displayName === 'VDC_IMCA')?.displayValue;
+        const section = props.find(c => c.displayName === 'VDC_AISC')?.displayValue;
         const Length = props.find(a => a.displayName === 'VDC_LENGTH')?.displayValue;
         const weight = props.find(y => y.displayName === 'VDC_WEIGHT')?.displayValue;
-        const type = props.find(x => x.displayName === 'VDC_TYPE_NAME')?.displayValue;
+        const part = props.find(x => x.displayName === 'Comments')?.displayValue;
         
-        return { dbid, name,EJES,preco,units,s_units,section,Length,weight,category,type };
+        return { dbid, name,mass,preco,Imca,section,Length,weight,category,part };
     },
     onRowClick: (row, viewer) => {
         viewer.isolate([row.dbid]);
@@ -69,7 +67,7 @@ export class DataGridPanel extends Autodesk.Viewing.UI.DockingPanel {
             groupBy: DATAGRID_CONFIG.groupBy,
             rowClick: (e, row) => DATAGRID_CONFIG.onRowClick(row.getData(), this.extension.viewer)
         });
-        this.table.setFilter("preco","=",1);
+        //this.table.setFilter("preco","=",1);
         this.exportbutton = document.createElement('button');
         //this.importbutton.type ='file';
         this.exportbutton.innerHTML = 'EXPORT XLSX';
